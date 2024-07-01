52d22eb5cce18213a8e55b6f09e67f36b1b7e14d
Aryan Swarnakar <125438106+AryanSwar@users.noreply.github.com>
2024-07-01 19:49:04 +0530

    Add files via upload

---
+++ Pro2.java
@@ -0,0 +1,10 @@
+public class Pro2 {
+    public static void main(String args[]){
+        System.out.println("The sum of these number");
+        int num1 = 3;
+        int num2 = 4;
+        int num3 = 6;
+        int sum = num1 + num2 + num3;
+        System.out.print(sum);
+    }
+}
