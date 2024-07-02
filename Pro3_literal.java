a4d99b6d76d057040aceb4afe4c6f7562f10f1c8
Aryan Swarnakar <125438106+AryanSwar@users.noreply.github.com>
2024-07-02 08:15:49 +0530

    Add files via upload

---
+++ Pro3_literal.java
@@ -0,0 +1,15 @@
+public class Pro3_literal {
+    public static void main(String []args){
+        byte age = 34;
+        int age2 = 46;
+        short age3 = 87;
+        long age4 = 878768654434655643l;
+        char ch = 'A';
+        float f1 = 5.6f;
+        double d1 = 4.66;
+        boolean a = true;
+        System.out.println(age);
+        String str = "Aryan";
+        System.out.println(str);
+    }
+}
