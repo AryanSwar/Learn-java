3a5489bbd43886cffd5cc4eb1de80682ca619b20
Aryan Swarnakar <125438106+AryanSwar@users.noreply.github.com>
2024-07-03 10:41:51 +0530

    Add files via upload

---
+++ Pro7_ch02_optrater_assign.java
@@ -0,0 +1,35 @@
+public class Pro7_ch02_optrater_assign {
+    public static void main(String args[]){
+      int a = 3;
+    //  '%' this sign give reminder
+    //   int b = 7 % a;
+        // Assignment operator
+    //    int b += 2;
+    //    int b = 4;
+    //    b += 3;
+    //    b *= 3;
+       
+    //   System.out.println(b);
+        
+    // comparison operator
+        System.out.println(6==8);
+        System.out.println(6>=8);
+        System.out.println(6<=8);
+        System.out.println(6 != 8);
+
+        System.out.println(64>5 && 64>98);//And operator
+        System.out.println(64<5 || 64<8);//or
+        
+        
+        // Bitwise operator
+        System.out.println(2&3);
+
+//      10
+//      11
+//      ----
+//      10
+
+
+    }
+    
+}
